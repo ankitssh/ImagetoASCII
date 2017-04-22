5cdbfc8b616017e6568b9629dcc24c367c4d7b78
soulxhacker <godotcoffee231@gmail.com>
2017-04-22 10:48:37 +0530

    Initial commit

---
+++ ImagetoASCII/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace ImagetoASCII
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitApp = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imagePreview = new System.Windows.Forms.PictureBox();
+            this.imageHistory = new System.Windows.Forms.ListBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.saveProgressBar = new System.Windows.Forms.ToolStripProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePreview)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(212, 28);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(211, 24);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(801, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFile,
+            this.saveFile,
+            this.quitApp});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // openFile
+            // 
+            this.openFile.Name = "openFile";
+            this.openFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openFile.Size = new System.Drawing.Size(213, 24);
+            this.openFile.Text = "&Open Image";
+            this.openFile.Click += new System.EventHandler(this.openFile_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Name = "saveFile";
+            this.saveFile.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveFile.Size = new System.Drawing.Size(213, 24);
+            this.saveFile.Text = "&Save";
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // quitApp
+            // 
+            this.quitApp.Name = "quitApp";
+            this.quitApp.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Q)));
+            this.quitApp.Size = new System.Drawing.Size(213, 24);
+            this.quitApp.Text = "&Quit";
+            this.quitApp.Click += new System.EventHandler(this.quitApp_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(62, 24);
+            this.helpToolStripMenuItem.Text = "About";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // imagePreview
+            // 
+            this.imagePreview.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.imagePreview.InitialImage = null;
+            this.imagePreview.Location = new System.Drawing.Point(12, 69);
+            this.imagePreview.Name = "imagePreview";
+            this.imagePreview.Size = new System.Drawing.Size(477, 461);
+            this.imagePreview.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imagePreview.TabIndex = 2;
+            this.imagePreview.TabStop = false;
+            this.imagePreview.DragDrop += new System.Windows.Forms.DragEventHandler(this.imagePreview_DragDrop);
+            this.imagePreview.DragEnter += new System.Windows.Forms.DragEventHandler(this.imagePreview_DragEnter);
+            // 
+            // imageHistory
+            // 
+            this.imageHistory.FormattingEnabled = true;
+            this.imageHistory.ItemHeight = 16;
+            this.imageHistory.Location = new System.Drawing.Point(510, 69);
+            this.imageHistory.Name = "imageHistory";
+            this.imageHistory.Size = new System.Drawing.Size(279, 452);
+            this.imageHistory.TabIndex = 3;
+            this.imageHistory.SelectedIndexChanged += new System.EventHandler(this.imageHistory_SelectedIndexChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveProgressBar});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 533);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(801, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // saveProgressBar
+            // 
+            this.saveProgressBar.Name = "saveProgressBar";
+            this.saveProgressBar.Size = new System.Drawing.Size(100, 16);
+            this.saveProgressBar.ToolTipText = "Saving";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(543, 533);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Recent";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(801, 555);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.imageHistory);
+            this.Controls.Add(this.imagePreview);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Image 2 ASCII";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imagePreview)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFile;
+        private System.Windows.Forms.ToolStripMenuItem saveFile;
+        private System.Windows.Forms.ToolStripMenuItem quitApp;
+        private System.Windows.Forms.PictureBox imagePreview;
+        private System.Windows.Forms.ListBox imageHistory;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar saveProgressBar;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ImagetoASCII/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ImagetoASCII
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog fd = new OpenFileDialog(); //Open File Dialog Box to import images
+        Dictionary<string, string> previousFilePath = new Dictionary<string, string>(); //Holds the Filename and filepath of images
+        private string pixels = "$&#MHGw*+-. "; //Characters to replace the pixels according to their brightness
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            imagePreview.AllowDrop = true;
+            label1.Text = "Drag n Drop an image or use File Open";
+          
+           
+        }
+
+        private void openFile_Click(object sender, EventArgs e)
+        {
+            
+            fd.Title = "Open File";
+            fd.Filter ="Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+            fd.DefaultExt = ".jpg";
+
+            if (fd.ShowDialog() == DialogResult.OK&&!imageHistory.Items.Contains(fd.SafeFileName))
+            {
+               
+              
+                imageHistory.Items.Insert(0,fd.SafeFileName); //Inserts the latest file at the top
+                imagePreview.ImageLocation = fd.FileName; //Changes the picture box image to the location of latest image
+                previousFilePath.Add(fd.SafeFileName,fd.FileName); 
+                
+            
+            }
+        }
+
+        private void quitApp_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "MyASCIIArt.txt";
+            sfd.Title = "Save File";
+            sfd.Filter = "Text Files | *.txt";
+            sfd.DefaultExt = ".txt";
+            if (imagePreview.ImageLocation == null)
+            {
+                //Can't save image without having an image first
+                MessageBox.Show("Please open an image first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+
+            }
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+
+                //ASCII Conversion Start
+                try
+                {
+                    int percentHeight=0;
+                    int percentWidth = 0;
+
+                    var img = new Bitmap(imagePreview.ImageLocation);
+                    if (img.Width > img.Height)
+                    {
+
+                         percentHeight = img.Height - (int)(img.Height * (30.00 / 100.00));
+                         percentWidth = img.Width - (int)(img.Width * (70.00 / 100.00));
+                    }
+                    else if (img.Width < img.Height)
+                    {
+                        percentHeight = img.Height - (int)(img.Height * (70.00 / 100.00));
+                        percentWidth = img.Width - (int)(img.Width * (30.00 / 100.00));
+
+                    }
+                    else
+                    {
+                        percentHeight = img.Height - (int)(img.Height * (50.00 / 100.00));
+                        percentWidth = img.Width - (int)(img.Width * (50.00 / 100.00));
+                    
+                    }
+
+
+
+
+                    img = new Bitmap(img, percentHeight, percentWidth); //Decreasing the Height and Width of the image
+                    saveProgressBar.Minimum = 0;
+                    saveProgressBar.Maximum = img.Height;
+
+                    for (int y = 0; y < img.Height; y++)
+                    {
+                        for (int x = 0; x < img.Width; x++)
+                        {
+                            var color = img.GetPixel(x, y); //Reading color of each pixel on the image
+                            var brightness = Brightness(color); //Get the lightness or darkness of the color
+                            var idx = brightness / 255 * (pixels.Length - 1); //Help us get a value to pick a character from the pixel string
+                            var pxl = pixels[(int)Math.Round(idx)];
+                            sw.Write(pxl);//Writing the pixel
+
+
+
+                        }
+                        saveProgressBar.Value = saveProgressBar.Value + 1; //Increasing the progress bar
+
+                        sw.WriteLine();
+
+                    }
+                    saveProgressBar.Value = 0; //Resets the progress bar
+                    sw.Close();
+                }
+
+
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Uh-oh! Something unexpected happened. Please check Log file for more info.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    StreamWriter sr = new StreamWriter("Log.txt", true);
+                    sr.WriteLine(DateTime.Now + "\t" + ex.Message);
+                    sr.Close();
+
+
+                }
+
+                //ASCII Conversion End
+
+            }
+        }
+        private static double Brightness(Color c)
+        {
+            return Math.Sqrt(
+                c.R*c.R*.241 +
+                c.G*c.G*.691+
+                c.B*c.B*.068
+                );
+ 
+        }
+
+       
+
+        private void imageHistory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           imagePreview.ImageLocation=previousFilePath[imageHistory.SelectedItem.ToString()]; //Click on an image in Recent to load it
+
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Created By\n Ankit Sharma (Soul_Hacker)\n Software is for non-commercial use only","About",MessageBoxButtons.OK,MessageBoxIcon.Information);
+        }
+
+        private void imagePreview_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
+        private void imagePreview_DragDrop(object sender, DragEventArgs e)
+        {
+            //The drag and drop code 
+            try
+            {
+                string dragLocation = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+                string safeFileName = dragLocation.Substring(1 + dragLocation.LastIndexOf('\\'));
+                if (dragLocation == null || imageHistory.Items.Contains(safeFileName))
+                    return;
+
+                imagePreview.ImageLocation = dragLocation;
+                imageHistory.Items.Insert(0, safeFileName);
+                previousFilePath.Add(safeFileName, dragLocation);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Uh-oh! Something unexpected happened. Please check Log file for more info.","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+                StreamWriter sr = new StreamWriter("Log.txt", true);
+                sr.WriteLine(DateTime.Now + "\t" + ex.Message);
+                sr.Close();
+            }
+
+           
+            
+        }
+    }
+}
